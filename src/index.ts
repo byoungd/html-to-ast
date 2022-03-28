12f77f3e284eb86b6927bc63830d848f5100ae97
byoungd <pg98@vip.qq.com>
2022-03-28 18:59:36 +0800

    fix: fix wrong export htmlVoidElements

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
+import { htmlVoidElements } from './element';
 import { parse } from './parse';
 import { stringify } from './stringify';
-import { htmlVoidElements } from 'html-void-elements';
 
 export { parse, stringify, htmlVoidElements };
